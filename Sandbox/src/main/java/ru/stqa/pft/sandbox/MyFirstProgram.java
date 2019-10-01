e68fcb293d348c7a2249323763982a96957aafad
Dedulkaa <55138703+Dedulkaa@users.noreply.github.com>
2019-10-01 15:53:36 +0300

    new

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+  public static void main(String[] args){
+  hello("world");
+  hello("user");
+  hello("alexei");
+
+  Square s = new Square(5);
+    System.out.println("Square with side  " + s.l + " = " + s.area());
+
+  Rectangle r = new Rectangle (4,6);
+    System.out.println("Square of rectangle with side  " + r.a +" and " + r.b + " = " + r.area());
+}
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + " !");
+}
+
+}
